f40e07d38c4ff0dd3b0dbb04fd4e4998e82b13be
Bikram <bikram@FH-MAC-19-055.local>
2020-04-13 10:31:29 +0530

    app class and function versions created separately

---
+++ src/AppFunction.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function App() {
+    const [count, setCount] = useState(0);
+    const incrementCount = () => {
+        setCount(prevCount => prevCount + 1);
+    }
+    return (
+        <button onClick={incrementCount}>
+            I was clicked {count} times
+        </button >
+    );
+}
+
+export default App;
