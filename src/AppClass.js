d4df6af945216693534b2e4c024beabed424f56a
Bikram <bikram@FH-MAC-19-055.local>
2020-04-13 10:42:41 +0530

    toggle state demonstration on class and functional components

--- src/AppClass.js
+++ src/AppClass.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {
-    count: 0
+    count: 0,
+    isOn: false
   }
 
   incrementCount = () => {
     this.setState(prevState => ({ count: prevState.count + 1 }));
   }
 
+  toggleLight = () => {
+    this.setState(prevState => ({ isOn: !prevState.isOn }));
+  }
+
   render() {
+    const { isOn, count } = this.state;
     return (
-      <button onClick={this.incrementCount}>
-        I was clicked {this.state.count} times
-    </button >
+      <>
+        <h2>Counter</h2>
+        <button onClick={this.incrementCount}>
+          I was clicked {count} times
+        </button >
+
+        <h2>Toggle Light</h2>
+        <img
+          src={
+            isOn
+              ? "https://icon.now.sh/highlight/fd0"
+              : "https://icon.now.sh/highlight/aaa"
+          }
+          style={{
+            height: "50px",
+            width: "50px"
+          }}
+          alt="flashLight"
+          onClick={this.toggleLight}
+        />
+      </>
     );
   }
 }
--- src/AppFunction.js
+++ src/AppFunction.js
@@ -2,13 +2,37 @@ import React, { useState } from 'react';
 
 function App() {
     const [count, setCount] = useState(0);
+    const [isOn, setIsOn] = useState(false);
+
     const incrementCount = () => {
         setCount(prevCount => prevCount + 1);
     }
+
+    const toggleLight = () => {
+        setIsOn(prevFlag => !prevFlag);
+    }
     return (
-        <button onClick={incrementCount}>
-            I was clicked {count} times
-        </button >
+        <>
+            <h2>Counter</h2>
+            <button onClick={incrementCount}>
+                I was clicked {count} times
+            </button >
+
+            <h2>Toggle Light</h2>
+            <img
+                src={
+                    isOn
+                        ? "https://icon.now.sh/highlight/fd0"
+                        : "https://icon.now.sh/highlight/aaa"
+                }
+                style={{
+                    height: "50px",
+                    width: "50px"
+                }}
+                alt="flashLight"
+                onClick={toggleLight}
+            />
+        </>
     );
 }
 
