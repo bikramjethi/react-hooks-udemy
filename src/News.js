7608486cd8d34c689053c485ad63b8a31673f47a
Bikram <bikram@FH-MAC-19-055.local>
2020-04-28 23:20:38 +0530

    search results based on input value

--- src/News.js
+++ src/News.js
@@ -3,22 +3,21 @@ import axios from 'axios';
 
 export default function News() {
     const [results, setResults] = useState([]);
+    const [query, setQuery] = useState("");
     useEffect(() => {
-        // .then(response => {
-        //     console.log('control here 2');
-        //     console.log(response);
-        //     setResults(response.data.hits);
-        // })
         getResults();
-        return () => {}
-    }, []);
+    }, [query]);
 
     const getResults = async () => {
-        const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks');
+        const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
         setResults(response.data.hits);
     }
 
     return <>
+        <input
+            type="text"
+            onChange={event => setQuery(event.target.value)}
+        />
         <ul>
             {results.map(result => (
                 <li key={result.objectID}>
