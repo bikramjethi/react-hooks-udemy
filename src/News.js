f97b3d49a096cfa01ab56e765943821753eb3316
Bikram <bikram@FH-MAC-19-055.local>
2020-04-28 23:14:09 +0530

    async/await with useEffect hook

--- src/News.js
+++ src/News.js
@@ -4,13 +4,20 @@ import axios from 'axios';
 export default function News() {
     const [results, setResults] = useState([]);
     useEffect(() => {
-        axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
-            .then(response => {
-                console.log('control here 2');
-                console.log(response);
-                setResults(response.data.hits);
-            })
-    }, [])
+        // .then(response => {
+        //     console.log('control here 2');
+        //     console.log(response);
+        //     setResults(response.data.hits);
+        // })
+        getResults();
+        return () => {}
+    }, []);
+
+    const getResults = async () => {
+        const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks');
+        setResults(response.data.hits);
+    }
+
     return <>
         <ul>
             {results.map(result => (
