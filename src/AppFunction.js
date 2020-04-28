3c0cd42b5d56f36606de11d5c1c9d0fc6d769db0
Bikram <bikram@FH-MAC-19-055.local>
2020-04-28 17:03:54 +0530

    clean up listeners which don't have a supportive API

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
+const initialLocationState = {
+    latitude: null,
+    longitude: null,
+    speed: null
+}
+
 function App() {
     const [count, setCount] = useState(0);
     const [isOn, setIsOn] = useState(false);
     const [mousePosition, setMousePosition] = useState({ x: null, y: null });
     const [status, setStatus] = useState(navigator.onLine);
+    const [location, setLocation] = useState(initialLocationState);
+    let mounted = true;
 
     const incrementCount = () => {
         setCount(prevCount => prevCount + 1);
@@ -26,11 +34,15 @@ function App() {
         window.addEventListener('mousemove', handleMouseMove);
         window.addEventListener('online', handleOnLine);
         window.addEventListener('offline', handleOffLine);
+        navigator.geolocation.getCurrentPosition(handleGeolocation);
+        const watchId = navigator.geolocation.watchPosition(handleGeolocation);
 
         return () => {
             window.removeEventListener('mousemove', handleMouseMove);
             window.removeEventListener('online', handleOnLine);
             window.removeEventListener('offline', handleOffLine);
+            navigator.geolocation.clearWatch(watchId);
+            mounted = false;
         }
     }, [count]);
 
@@ -40,6 +52,16 @@ function App() {
 
     const handleOffLine = () => {
         setStatus(false);
+    }
+
+    const handleGeolocation = event => {
+        if (mounted) {
+            setLocation({
+                latitude: event.coords.latitude,
+                longitude: event.coords.longitude,
+                speed: event.coords.speed
+            })
+        }
     }
 
     return (
@@ -69,6 +91,11 @@ function App() {
 
             <h2>Network status</h2>
             <p>You are {status ? "online" : "offline"}</p>
+
+            <h2>Geolocation</h2>
+            <p>Latitude is {location.latitude}</p>
+            <p>Longitude is {location.longitude}</p>
+            <p>Your speed is {location.speed ? location.speed : "0"}</p>
         </>
     );
 }
