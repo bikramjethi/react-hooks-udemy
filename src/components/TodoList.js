26e573b2c488724c7fe8393cabf09a6eb3a1a90c
Bikram <bikram@FH-MAC-19-055.local>
2020-04-29 11:30:05 +0530

    styling todo list with tailwind css

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,12 +3,30 @@ import TodosContext from '../context';
 
 export default function TodoList() {
     const { state } = useContext(TodosContext);
+    const title = state.todos.length > 0 ? `${state.todos.length} Todos` : 'Nothing to do!!!';
 
-    return <div>
-        <ul>
+    return <div className="container m-auto text-center font-mono">
+        <h1 className="text-3xl">{title}</h1>
+        <ul className="text-white p-0">
             {state.todos.map(todo => (
-                <li key={todo.id}>
-                    <span>{todo.text}</span>
+                <li key={todo.id}
+                    className="flex items-center bg-orange-500 border-black border-dashed border-2 my-2 py-4"
+                >
+                    <span className="cursor-pointer flex-1 ml-12">{todo.text}</span>
+                    <button>
+                        <img
+                            src="https://icon.now.sh/edit/0050c5"
+                            alt="Edit Icon"
+                            className="h6"
+                        />
+                    </button>
+                    <button>
+                        <img
+                            src="https://icon.now.sh/delete/8b0000"
+                            alt="Delete Icon"
+                            className="h6"
+                        />
+                    </button>
                 </li>
             ))}
         </ul>
